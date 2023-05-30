0a87f8f5509c51d2df7be98435c5435f1bab72e3
marcelo.cmendes <marcelo.cmendes@americanas.io>
2023-05-30 01:21:39 -0300

    feat: login user

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ function oneUserExist(data) {
 }
 
 // Register New User
-server.post('/auth/registe', (req, res) => {
+server.post('/auth/register', (req, res) => {
     const { email, password, nome, descricaoPerfil, github } = req.body;
 
     fs.readFile("./db.json", (err, data) => {
@@ -82,15 +82,69 @@ server.post('/auth/registe', (req, res) => {
                 return
             } else {
                 const status = 200
-                console.log(result)
-                const message = JSON.stringify(result)
-                res.status(status).json({ status, message })
+                const access_token = createToken({ email, password })
+                const users = data.users.filter((item) => item.id === newId)[0]
+
+                res.status(status).json({ status, access_token, users })
                 return
             }
         });
     });
-
 })
+
+// Login to one of the users from ./users.json
+server.post('/auth/login', (req, res) => {
+    const { email, password } = req.body;
+    fs.readFile("./db.json", (err, data) => {
+        var data = JSON.parse(data.toString());
+
+        if (err) {
+            const status = 401
+            const message = err
+            res.status(status).json({ status, message })
+            return
+        };
+
+        if (isAuthenticated({ email, password, data }) === false) {
+            const status = 401
+            const message = 'Incorrect email or password'
+            res.status(status).json({ status, message })
+            return
+        }
+
+        const status = 200
+        const access_token = createToken({ email, password })
+        const users = data.users.filter((item) => item.email === email)[0]
+
+        res.status(status).json({ status, access_token, users })
+    });
+})
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+    if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+        const status = 401
+        const message = 'Error in authorization format'
+        res.status(status).json({ status, message })
+        return
+    }
+    try {
+        let verifyTokenResult;
+        verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+        if (verifyTokenResult instanceof Error) {
+            const status = 401
+            const message = 'Access token not provided'
+            res.status(status).json({ status, message })
+            return
+        }
+        next()
+    } catch (err) {
+        const status = 401
+        const message = 'Error access_token is revoked'
+        res.status(status).json({ status, message })
+    }
+})
+
 
 // CONFIG PORTA DE ENTRADA PARA O SERVER
 server.use(router)
